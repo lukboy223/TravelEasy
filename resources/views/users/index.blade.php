92bfae550cb8d32a67aaab17e6661eb5ffa81b24
lukboy223 <ltvanekeren@gmail.com>
2025-02-12 16:08:10 +0100

    kleine styling update

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -13,13 +13,13 @@
             <thead>
                 <tr>
                     <th
-                        class="px-4 py-2 border-b-2 border-gray-300 dark:border-gray-700 text-left leading-4 text-white tracking-wider">
+                        class="px-4 py-2 border-b-2 border-r border-gray-300 dark:border-gray-700 text-left leading-4 text-white tracking-wider">
                         Gebruikersnaam</th>
                     <th
-                        class="px-4 py-2 border-b-2 border-gray-300 dark:border-gray-700 text-left leading-4 text-white tracking-wider">
+                        class="px-4 py-2 border-b-2 border-r border-gray-300 dark:border-gray-700 text-left leading-4 text-white tracking-wider">
                         Aangemaakt</th>
                     <th
-                        class="px-4 py-2 border-b-2 border-gray-300 dark:border-gray-700 text-left leading-4 text-white tracking-wider">
+                        class="px-4 py-2 border-b-2 border-r border-gray-300 dark:border-gray-700 text-left leading-4 text-white tracking-wider">
                         Wijzigen</th>
                     <th
                         class="px-4 py-2 border-b-2 border-gray-300 dark:border-gray-700 text-left leading-4 text-white tracking-wider">
@@ -37,11 +37,11 @@
                 {{-- shows the data of the given array --}}
                 @foreach($users as $user)
                 <tr class="bg-white dark:bg-gray-800">
-                    <td class="px-4 py-2 border-b border-gray-300 dark:border-gray-700 text-white">{{ $user->name }}
+                    <td class="px-4 py-2 border-b border-gray-300 dark:border-gray-700 text-white border-r">{{ $user->name }}
                     </td>
-                    <td class="px-4 py-2 border-b border-gray-300 dark:border-gray-700 text-white">{{ $user->created_at
+                    <td class="px-4 py-2 border-b border-gray-300 dark:border-gray-700 text-white border-r">{{ $user->created_at
                         }}</td>
-                    <td class="px-4 py-2 border-b border-gray-300 dark:border-gray-700 text-white"><a href=""
+                    <td class="px-4 py-2 border-b border-gray-300 dark:border-gray-700 text-white border-r"><a href=""
                             class="bg-green-700 p-1 rounded">Wijzigen</a></td>
                     <td class="px-4 py-2 border-b border-gray-300 dark:border-gray-700 text-white"><a href=""
                             class="bg-red-700 p-1 rounded">Verwijderen</a></td>
